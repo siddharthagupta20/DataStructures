337b3c8521f32eb39e2f65f351f996e2e237dde4
siddharthagupta20 <siddharthagupta20@users.noreply.github.com>
2020-10-11 20:11:48 +0530

    [Siddhartha] modified: feature of adding in LinkedList

--- src/main/java/com/cg/ds/LinkedList.java
+++ src/main/java/com/cg/ds/LinkedList.java
@@ -3,21 +3,29 @@ package com.cg.ds;
 public class LinkedList<T> {
 	private INode<T> head;
 	private INode<T> tail;
-	
+
 	public LinkedList() {
-		this.head=null;
-		this.tail=null;
+		this.head = null;
+		this.tail = null;
 	}
+
+	public INode<T> getHead() {
+		return head;
+	}
+
+	public INode<T> getTail() {
+		return tail;
+	}
+
 	public void add(INode<T> node) {
-		if(this.head==null) { 
-			this.head=node;
-			this.tail=node;
-		}
-		else {
+		if (this.head == null) {
+			this.head = node;
+			this.tail = node;
+		} else {
 			this.head.setNext(node);
-			this.head=node;
+			this.head = node;
 		}
-			
+
 	}
 
 }
--- src/test/java/com/cg/ds/LinkedListTest.java
+++ src/test/java/com/cg/ds/LinkedListTest.java
@@ -1,5 +1,21 @@
 package com.cg.ds;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class LinkedListTest {
+	
+	@Test
+	public void whenNodesAddedShouldReturnLast() {
+		LinkedList<Integer> list=new LinkedList<Integer>();
+		Node<Integer> firstNode=new Node<Integer>(50);
+		Node<Integer> secondNode=new Node<Integer>(30);
+		Node<Integer> thirdNode=new Node<Integer>(40);
+		list.add(firstNode);
+		list.add(secondNode);
+		list.add(thirdNode);
+		assertEquals(firstNode, list.getTail());
+	}
 
 }
